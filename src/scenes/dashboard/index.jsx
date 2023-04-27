ad26d12dda40e545704279864b89ee0e8a1f067c
Thebks <ichbks@gmail.com>
2023-04-27 17:25:09 +0500

    fullDashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -44,7 +44,7 @@ const Dashboard = () => {
                 <Box gridColumn="span 3" backgroundColor={colors.primary[400]} display="flex" alignItems="center" justifyContent="center">
                     <StatBox
                         title="28,324"
-                        subtitle="Sales Done"
+                        subtitle="Emails"
                         progress="0.50"
                         increase="21%"
                         icon={
@@ -73,6 +73,20 @@ const Dashboard = () => {
                     <StatBox
                         title="185,324"
                         subtitle="Sales Done"
+                        progress="0.80"
+                        increase="41%"
+                        icon={
+                            <TrafficIcon
+                                sx={{ color: colors.greenAccent[600] }} fontSize="26px"
+                            />
+                        }
+                    />
+                </Box>
+
+                <Box gridColumn="span 3" backgroundColor={colors.primary[400]} display="flex" alignItems="center" justifyContent="center">
+                    <StatBox
+                        title="18,435,324"
+                        subtitle="Traffic"
                         progress="0.80"
                         increase="41%"
                         icon={
@@ -117,7 +131,83 @@ const Dashboard = () => {
                             Recent Transactions
                         </Typography>
                     </Box>
-
+                    {dummyTransactions.map((transaction, i) => {
+                        return (
+                            <Box
+                                key={`${transaction.txId} -${i}`}
+                                display="flex"
+                                justifyContent="space-between"
+                                alignItems="center"
+                                borderBottom={`4px solid ${colors.primary[500]}`}
+                                p="15px"
+                            >
+                                <Box>
+                                    <Typography color={colors.greenAccent[500]} variant="h5" fontWeight="600">
+                                        {transaction.txId}
+                                    </Typography>
+                                    <Typography>
+                                        {transaction.user}
+                                    </Typography>
+                                </Box>
+                                <Box color={colors.grey[100]}>
+                                    {transaction.date}
+                                </Box>
+                                <Box backgroundColor={colors.greenAccent[500]} p="5px 10px" borderRadius="4px">
+                                    ${transaction.cost}
+                                </Box>
+                            </Box>
+                        )
+                    })}
+                </Box>
+                {/* ROW3 */}
+                <Box gridColumn="span 4"
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]}
+                    p="30px"
+                >
+                    <Typography variant="h5" fontWeight="600">
+                        campaign
+                    </Typography>
+                    <Box display="flex"
+                        flexDirection="column"
+                        alignItems="center"
+                        mt="25px"
+                    >
+                        <ProgressCircle size="125" />
+                        <Typography
+                            variant="h5"
+                            color={colors.greenAccent[500]}
+                            sx={{ mt: "15px" }}
+                        >
+                            $48,352 revenue generated
+                        </Typography>
+                        <Typography>Includes extra costs</Typography>
+                    </Box>
+                </Box>
+                <Box
+                    gridColumn="span 4"
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]}
+                >
+                    <Typography variant="h5" fontWeight="600" sx={{ padding: "30px 30px 0 30px" }}>
+                        Sales Quantity
+                    </Typography>
+                    <Box mt="-20px" height="250px">
+                        <BarChart isDashboard={true} />
+                    </Box>
+                </Box>
+                <Box
+                    gridColumn="span 4"
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]}
+                    padding="30px"
+                >
+                    <Typography variant="h5" fontWeight="600" sx={{ marginBottom: "15px" }}>
+                        Geo Traffic
+                    </Typography>
+                    <Box height="200px">
+                        <GeoChart isDashboard={true} />
+                    </Box>
                 </Box>
             </Box>
         </Box>
